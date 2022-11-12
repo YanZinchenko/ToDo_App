2176d72ef2be4a776ede75fc866c5be6c8ec811d
Y@n <Zinchenko.yan@gmail.com>
2022-11-12 20:33:58 +0200

    Add react-icon

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,7 +1,35 @@
 import React from 'react';
+import TodoItem from './item/TodoItem';
+const date = [
+    {
+        _id: 'qwer11',
+        title: 'Finish the essay collaboration',
+        isCompleted: false,
+    },
+    {
+        _id: 'qwer113123',
+        title: 'Read next chapter of the book',
+        isCompleted: false,
+    },
+    {
+        _id: 'qwer11567',
+        title: 'Send the finished assignment',
+        isCompleted: false,
+    },
+];
 
 const Home = () => {
-    return <div className='bg-gray-900 h-screen text-white'>Home</div>;
+    const [todos, setTodos] = useState(date);
+    return (
+        <div className=' text-white w-4/5 mx-auto'>
+            <h1 className='text-2xl font-bold text-center mb-8'>
+                Todo for junior
+            </h1>
+            {todos.map((todo) => (
+                <TodoItem key={todo._id} todo={todo} />
+            ))}
+        </div>
+    );
 };
 
 export default Home;
--- src/index.jsx
+++ src/index.jsx
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Home from './components/Home/Home';
 import './index.css';
+import Layout from './components/layout/Layout';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <Home />
+        <Layout>
+            <Home />
+        </Layout>
     </React.StrictMode>
 );
